474e4e223586cc967a06715d0e1bf55345a4e5c3
Hubert Arciszewski <hubertarciszewski@gmail.com>
2019-03-14 09:14:59 +0100

    Add chickenChoop file

---
+++ chickenChoop.py
@@ -0,0 +1,26 @@
+class Chicken:
+
+    total_eggs = 0
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.eggs = 0
+
+    def lay_egg(self):
+        Chicken.total_eggs += 1
+        self.eggs += 1
+        return f"Chicken {self.name} lay {self.eggs} eggs"
+
+
+c1 = Chicken(name="Alice", species="Green leg")
+c2 = Chicken(name="Arthur", species="Black tail")
+
+print(Chicken.total_eggs)  # 0
+
+print(c1.lay_egg())  # 1
+print(Chicken.total_eggs)  # 1
+
+print(c2.lay_egg())  # 1
+print(c2.lay_egg())  # 2
+print(Chicken.total_eggs)  # 3
